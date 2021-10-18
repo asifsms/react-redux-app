2ffb2640e5c3db2861a69d848e5057b019b05aa6
Asif <asif.ms@acodez.co.in>
2021-10-18 22:00:34 +0530

    Display Data using fake API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productID" component={ProductDetails} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </Router> 
     </div>
   );
 }
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,18 +1,12 @@
 import { ActionTypes } from "../constants/action-types";
 
 const initialState = {
-    products: [
-        {
-            id: 1,
-            title: "Asif",
-            Group: "B+",
-        },
-    ],
+    products: [],
 };
 export const productReducer = (state = initialState, { type, payload }) => {
     switch (type ) {
-        case ActionTypes.SELECTED_PRODUCTS:
-            return state;
+        case ActionTypes.SET_PRODUCTS:
+            return { ...state, products:payload };
         default:
             return state;
     }
